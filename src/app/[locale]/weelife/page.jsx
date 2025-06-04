62ebdd982596129eb07630a5287edc6653ba0535
İsmail Keskin <hamdismailkskn98@gmail.com>
2025-06-04 16:36:23 +0300

    aa

--- src/app/[locale]/weelife/page.jsx
+++ src/app/[locale]/weelife/page.jsx
@@ -1,6 +1,46 @@
+import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards";
 import WeelifeMain from "@/components/weelife";
 import React from "react";
 
-export default function Weelife() {
-  return <WeelifeMain />;
+export default async function Weelife() {
+  const test = [
+    {
+      amount: "197.00$",
+      name: "Pasha Pashayev",
+      date: "16/06/2024 10:47:28",
+    },
+    {
+      amount: "100.00$",
+      name: "Cavid Imanov",
+      date: "15/06/2024 21:05:13",
+    },
+    {
+
+      amount: "100.00$",
+      name: "Vedat AkgÜl",
+      date: "15/06/2024 18:56:34",
+    },
+    {
+      amount: "100.00$",
+      name: "Vusal Selimov",
+      date: "06/06/2024 13:15:25",
+    },
+    {
+      amount: "300.00$",
+      name: "Faiq Meherremli",
+      date: "13/06/2024 16:51:25",
+    },
+    {
+      amount: "200.00$",
+      name: "Şahsənəm Əliyeva",
+      date: "10/06/2024 16:54:25",
+    },
+  ];
+  return (
+    <>
+      <WeelifeMain>
+        <InfiniteMovingCards items={test} direction="right" speed="slow" />
+      </WeelifeMain>
+    </>
+  );
 }
--- src/components/ui/infinite-moving-cards.jsx
+++ src/components/ui/infinite-moving-cards.jsx
@@ -53,15 +53,15 @@ export const InfiniteMovingCards = ({ items, direction = "left", speed = "fast",
         {items.map((item, idx) => (
           <li
             className="relative w-[350px] max-w-full shrink-0 rounded-2xl border border-b-0 border-zinc-200 bg-[linear-gradient(180deg,#fafafa,#f5f5f5)] px-6 py-4 md:w-[350px] dark:border-zinc-700 dark:bg-[linear-gradient(180deg,#27272a,#18181b)]"
-            key={item.name}
+            key={idx}
           >
             <blockquote>
               <div aria-hidden="true" className="user-select-none pointer-events-none absolute -top-0.5 -left-0.5 -z-1 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"></div>
-              <span className="relative z-20 text-sm leading-[1.6] font-normal text-neutral-800 dark:text-gray-100">{item.quote}</span>
-              <div className="relative z-20 mt-6 flex flex-row items-center">
+              <span className="relative z-20 text-3xl leading-[1.6] font-normal text-neutral-800 dark:text-gray-100">{item.amount}</span>
+              <div className="relative z-20 mt-5 flex flex-row items-center">
                 <span className="flex flex-col gap-1">
                   <span className="text-sm leading-[1.6] font-normal text-neutral-500 dark:text-gray-400">{item.name}</span>
-                  <span className="text-sm leading-[1.6] font-normal text-neutral-500 dark:text-gray-400">{item.title}</span>
+                  <span className="text-sm leading-[1.6] font-normal text-neutral-500 dark:text-gray-400">{item.date}</span>
                 </span>
               </div>
             </blockquote>
